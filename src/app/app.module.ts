9e53286857cde78d2646bb296fc58415c58304c6
Crypthos <newlank1@hva.nl>
2020-07-22 01:21:53 +0200

    Added dashboard, chat server.js and app routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ChatComponent } from './chat/chat.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import {FormsModule} from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    ServerComponent,
+    ChatComponent,
+    DashboardComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
